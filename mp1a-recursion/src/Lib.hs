94772c5a5b1eade2233aa1854dad86995bb7ef73
charliemarshall3 <143765116+charliemarshall3@users.noreply.github.com>
2025-06-13 13:50:04 +0000

    mp1 complete

--- mp1a-recursion/src/Lib.hs
+++ mp1a-recursion/src/Lib.hs
@@ -54,77 +54,137 @@ rev xs = reverser xs []
     reverser (x:xs) acc = reverser xs (x:acc)
 
 --- ### app
-
+-- Write a function app :: [a] -> [a] -> [a] which appends two lists. 
+-- This function should behave like the Haskell built-in (++) :: [a] -> [a] -> [a], 
+-- and should run in linear time (in the size of the first list).
 -- don't forget to put the type declaration or you will lose points!
-app = undefined
+app :: [a] -> [a] -> [a]
+app [] ys = ys
+app (x:xs) ys = x : app xs ys
 
 --- ### inclist
 
 -- don't forget to put the type declaration or you will lose points!
-inclist = undefined
+-- Write a function inclist :: Num a => [a] -> [a] which adds 1 to each element of the input list.
+inclist :: Num a => [a] -> [a]
+inclist [] = []
+inclist (x:xs) = (x + 1) : inclist xs
 
 --- ### sumlist
-
+-- Write a function sumlist :: Num a => [a] -> a which adds all the elements of the input list.
 -- don't forget to put the type declaration or you will lose points!
-sumlist = undefined
+sumlist :: Num a => [a] -> a
+sumlist [] = 0
+sumlist (x:xs) = x + sumlist xs
 
 --- ### myzip
-
+-- Write a function myzip :: [a] -> [b] -> [(a,b)] which zips up the elements of two lists. 
+-- The resulting list should be the same length as the shorter of the two input lists.
 -- don't forget to put the type declaration or you will lose points!
-myzip = undefined
+myzip :: [a] -> [b] -> [(a,b)]
+myzip [] _ = []
+myzip _ [] = []
+myzip (x:xs) (y:ys) = (x,y) : myzip xs ys
 
 --- ### addpairs
-
+-- Write a function addpairs :: (Num a) => [a] -> [a] -> [a] which zips up two lists using the addition operator (+). 
+-- You must use your function myzip.
 -- don't forget to put the type declaration or you will lose points!
-addpairs = undefined
+addpairs :: (Num a) => [a] -> [a] -> [a]
+addpairs xs ys = addHelper (myzip xs ys) 
+  where
+      addHelper [] = []
+      addHelper ((x, y):xs) = x+y: addHelper xs
 
 --- ### ones
-
+-- Write a (constant) function ones :: [Integer] which produces an infinite list of the integer 1.
 -- don't forget to put the type declaration or you will lose points!
-ones = undefined
+ones :: [Integer]
+ones = 1:ones
 
 --- ### nats
-
+-- Write a (constant) function nats :: [Integer] which produces an infinite list of all the natural numbers starting at 0. 
+-- It is OK for this function if you do not use recursion.
 -- don't forget to put the type declaration or you will lose points!
-nats = undefined
+nats :: [Integer]
+nats = natsStart 0
+  where
+    natsStart n = n:natsStart (n+1)
 
 --- ### fib
-
+-- Write a (constant) function fib :: [Integer] which produces an infinite list of the Fibonacci series starting with numbers 0 and 1. 
+-- You can (and should) use your addpairs function here. This is the one place in the assignment where it really makes sense to use tail :: [a] -> [a].
 -- don't forget to put the type declaration or you will lose points!
-fib = undefined
+fib :: [Integer]
+fib = 0:1:addpairs fib (tail fib)
 
 --- Set Theory
 --- ----------
 
 --- ### add
-
+-- Write a function add :: Ord a => a -> [a] -> [a] which will add an element to the set. 
+-- Remember that it must add it in the correct place to ensure that the list remains sorted. 
+-- It should run in linear time (in the size of the list).
 -- don't forget to put the type declaration or you will lose points!
-add = undefined
+add :: Ord a => a -> [a] -> [a]
+add x [] = [x]
+add x (y:ys)
+  | x < y     = x : y : ys
+  | x == y    = y : ys
+  | otherwise = y : add x ys
 
 --- ### union
-
+-- Write a function union :: Ord a => [a] -> [a] -> [a] which unions two input sets (Haskell lists). 
+-- This should look similar to the "merge" step of merge-sort, and should run in linear time (in the added sizes of the input sets). 
+-- You may use the add function defined above, but if you do it probably will not run in linear time, so it's probably better not to.
 -- don't forget to put the type declaration or you will lose points!
-union = undefined
+union :: Ord a => [a] -> [a] -> [a]
+union xs [] = xs
+union [] ys = ys
+union (x:xs) (y:ys)
+  | x == y    = x: union xs ys
+  | x < y     = x: union xs (y:ys)
+  | otherwise = y: union (x:xs) ys
 
 --- ### intersect
-
+-- Write a function intersect :: Ord a => [a] -> [a] -> [a] which intersects two input sets. 
+-- This should run in linear time (in the size of the input sets).
 -- don't forget to put the type declaration or you will lose points!
-intersect = undefined
+intersect :: Ord a => [a] -> [a] -> [a]
+intersect xs [] = []
+intersect [] ys = []
+intersect (x:xs) (y:ys)
+  | x == y    = x: intersect xs ys
+  | x < y     = intersect xs (y:ys)
+  | otherwise = intersect (x:xs) ys 
 
 --- ### powerset
-
+-- Write a function powerset :: Ord a => [a] -> [[a]] which calculates the powerset of the input set. 
+-- Because the output is also a set, it must preserve our set properties, including that there are no duplicate elements and the elements are lexicographically sorted. 
+-- Using the functions union and add that you've already defined is useful here.
 -- don't forget to put the type declaration or you will lose points!
-powerset = undefined
+powerset :: Ord a => [a] -> [[a]]
+powerset [] = [[]]
+powerset (x:xs) =
+  let ps = powerset xs
+  in union ps (addElement x ps)
+
+addElement :: Ord a => a -> [[a]] -> [[a]]
+addElement _ [] = []
+addElement x (ys:yss) = add x ys : addElement x yss
+
 
 --- Higher Order Functions
 --- ----------------------
 
 --- ### inclist'
-
+-- Write a function inclist' :: Num a => [a] -> [a] which increments each element of an input list.
 -- don't forget to put the type declaration or you will lose points!
-inclist' = undefined
+inclist' :: Num a => [a] -> [a]
+inclist' = P.map (+1)
 
 --- ### sumlist'
-
+-- Write a function sumlist' :: (Num a) => [a] -> a which adds all the elements of the input list.
 -- don't forget to put the type declaration or you will lose points!
-sumlist' = undefined
+sumlist' :: Num a => [a] -> a
+sumlist' = P.foldr (+) 0
--- mp1b-adts/src/Lib.hs
+++ mp1b-adts/src/Lib.hs
@@ -23,39 +23,62 @@ data Exp = IntExp Integer
   deriving (Show, Eq)
 
 --- ### list2cons
-
+-- Write a function list2cons :: [a] -> List a which converts a Haskell list into our List type. 
+-- Do this recursively (not using higher-order functions).
 -- don't forget to put the type declaration or you will lose points!
-list2cons = undefined
+list2cons :: [a] -> List a
+list2cons [] = Nil
+list2cons (x:xs) = Cons x (list2cons xs)
 
 --- ### cons2list
-
+-- Write a function cons2list :: List a -> [a] which converts our List type into the Haskell list type. 
+-- Do this recursively.
 -- don't forget to put the type declaration or you will lose points!
-cons2list = undefined
+cons2list :: List a -> [a]
+cons2list Nil = []
+cons2list (Cons x xs) = x : cons2list xs
 
 --- ### eval
-
+-- Write a function eval :: Exp -> Integer which evaluates the integer expression represented by its input. 
+-- You may use recursion and higher-order functions.
 -- don't forget to put the type declaration or you will lose points!
-eval = undefined
+eval :: Exp -> Integer
+eval (IntExp n) = n
+eval (PlusExp exps) = sum (map eval exps)
+eval (MultExp exps) = product (map eval exps)
 
 --- ### list2cons'
-
+-- Write a function list2cons' :: [a] -> List a which converts a Haskell list into our List type. 
+-- You are required to use higher-order functions for this, no recursion.
 -- don't forget to put the type declaration or you will lose points!
-list2cons' = undefined
+list2cons' :: [a] -> List a
+list2cons' = foldr Cons Nil
 
 --- ### BinTree
-
+-- Write an ADT BinTree a which represents a binary tree that stores things of type a at internal nodes, and stores nothing at the leaves.
 -- BinTree
+data BinTree a = Leaf | Node a (BinTree a) (BinTree a)
+  deriving (Show, Eq)
 
 --- ### sumTree
-
+-- Write a function sumTree :: Num a => BinTree a -> a which takes a BinTree a (where a is a Num) from the previous problem and sums all the elements of its nodes.
 -- don't forget to put the type declaration or you will lose points!
-sumTree = undefined
+sumTree :: Num a => BinTree a -> a
+sumTree Leaf = 0
+sumTree (Node x left right) = x + sumTree left + sumTree right
 
 --- ### SimpVal
-
+-- Write an ADT SimpVal which represents the values that a simple programming language can have. We'll have IntVal for integers, BoolVal for booleans, StrVal for strings, and ExnVal for exceptions.
 -- SimpVal
+data SimpVal = IntVal Integer
+              | BoolVal Bool
+              | StrVal String
+              | ExnVal String
+  deriving (Show, Eq)
 
 --- ### liftIntOp
-
+-- Write a function\ liftIntOp :: (Integer -> Integer -> Integer) -> SimpVal -> SimpVal -> SimpVal\ which will take an operator over integers (like (+) :: Integer -> Integer -> Integer) and turn it into an operator over SimpVals. If the inputs are not IntVal, raise an exception by returning ExnVal "not an IntVal!".
 -- don't forget to put the type declaration or you will lose points!
-liftIntOp = undefined
+liftIntOp :: (Integer -> Integer -> Integer) -> SimpVal -> SimpVal -> SimpVal
+liftIntOp op (IntVal x) (IntVal y) = IntVal (op x y)
+liftIntOp _ _ _ = ExnVal "not an IntVal!"
